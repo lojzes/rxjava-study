ab0d662e8928860d821a74375c7d6790695737b0
liaoyuanyuan <liaoyuanyuan@taoche.com>
2019-05-05 17:48:08 +0800

    init

---
+++ src/main/java/com/lojzes/rxjava/Test.java
@@ -0,0 +1,13 @@
+package com.lojzes.rxjava;
+
+/**
+ * @author liaoyuanyuan
+ * @description
+ * @see
+ * @since 2019-05-05 17:43
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+}
